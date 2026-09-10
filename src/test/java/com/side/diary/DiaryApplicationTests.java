409b899d65b48da0b9108ed872326b08b5d17945
sakur35a <sakur35a@naver.com>
2026-09-10 15:24:09 +0900

    init commit

---
+++ src/test/java/com/side/diary/DiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.side.diary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiaryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
