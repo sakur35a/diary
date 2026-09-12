cd66ed01203379a116f60efcc5c3e158dc4c9d21
sakur35a <sakur35a@naver.com>
2026-09-12 19:47:15 +0900

    chore: 필요없어진 ResilienceConfig 삭제

--- src/main/java/com/side/diary/ResilienceConfig.kt
+++
@@ -1,6 +0,0 @@
-package com.side.diary
-
-import org.springframework.context.annotation.Configuration
-import org.springframework.resilience.annotation.EnableResilientMethods
-
-@Configuration @EnableResilientMethods class ResilienceConfig {}
